ed38df831ed9f5ec9739aa686c44010628455ee3
[Andrea De Filippi] <[andreadefilippi68@gmail.com]>
2025-10-16 11:18:07 +0200

    1.6.2 Projects accessibility fixes

--- js/index.js
+++ js/index.js
@@ -10,11 +10,17 @@ orbs.forEach((orb) => {
   orb.addEventListener('mouseenter', () => {
     orb.querySelector('.orb-anchor').classList.add('active');
   });
+  orb.querySelector('.orb-anchor').addEventListener('focusin', () => {
+    orb.classList.add('active');
+  });
 });
 
 orbs.forEach((orb) => {
   orb.addEventListener('mouseleave', () => {
     orb.querySelector('.orb-anchor').classList.remove('active');
+  });
+  orb.querySelector('.orb-anchor').addEventListener('focusout', () => {
+    orb.classList.remove('active');
   });
 });
 
--- js/projects.js
+++ js/projects.js
@@ -4,29 +4,56 @@ const closeButtons = document.querySelectorAll('.close-btn');
 
 let inactives;
 
-const hideCards = function () {
+const showCards = function () {
   inactives = projects.filter(
     (project) => !project.classList.contains('active')
   );
   inactives.forEach((inactive) => inactive.classList.add('inactive'));
 };
 
-const showCards = function () {
+const hideCards = function () {
   inactives.forEach((inactive) => inactive.classList.remove('inactive'));
+};
+
+const openProject = function () {
+  this.classList.add('active');
+  this.closest('.project').classList.add('active');
+  setTimeout(showCards, '0');
+  console.log(this);
+};
+
+const closeProject = function () {
+  this.closest('.project').classList.remove('active');
+  setTimeout(hideCards, '500');
 };
 
 projectFrontCards.forEach((card) => {
   card.addEventListener('click', () => {
-    card.classList.add('active');
-    card.closest('.project').classList.add('active');
-
-    setTimeout(hideCards, '0');
+    if (card.closest('.project').classList.contains('active')) {
+      closeProject.call(card);
+      document.activeElement.blur();
+    } else {
+      openProject.call(card);
+    }
+  });
+  card.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      if (card.closest('.project').classList.contains('active')) {
+        closeProject.call(card);
+        document.activeElement.blur();
+      } else {
+        openProject.call(card);
+      }
+    }
   });
 });
 
 closeButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    button.closest('.project').classList.remove('active');
-    setTimeout(showCards, '500');
+  button.addEventListener('click', closeProject.bind(button));
+  button.addEventListener('keydown', (e) => {
+    if (e.key === 'enter' || e.key === ' ') {
+      closeProject.call(button);
+      console.log(button);
+    }
   });
 });
